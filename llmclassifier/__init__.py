2e5a7e39c80844e2b17cca8437b1d3f4f0750e74
CVxTz <mansaryounessecp@gmail.com>
2024-11-07 19:06:14 +0100

    feat: setup.py

---
+++ llmclassifier/__init__.py
@@ -0,0 +1,5 @@
+from .llm_text_classifier import LLMTextClassifier
+
+__version__ = "0.1.0"
+
+__all__ = ["LLMTextClassifier"]
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='llmclassifier',
+    version='0.1.0',  # Update the version as needed
+    packages=find_packages(exclude=["tests.*", "tests"]), # Include all packages except test
+    author='Youness',
+    author_email='X@eY.Z',
+    description='LLM wrapper for text classification',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Important for README rendering on PyPI
+    url='https://github.com/CVxTz/llmclassifier',  # If you have a repo
+    install_requires=open('requirements.txt').readlines(),  # Reads dependencies from file
+    extras_require={"dev": open('requirements_dev.txt').readlines()},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Replace with your license
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9', # Or your required Python version
+    test_suite='tests', # for running tests via 'python setup.py test'
+)
