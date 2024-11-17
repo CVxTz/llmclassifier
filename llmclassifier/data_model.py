8cbde3d908b4dba6c802782d3b88cdf729075437
CVxTz <mansaryounessecp@gmail.com>
2024-11-17 12:50:40 +0100

    feat: handle literal_error

--- llmclassifier/data_model.py
+++ llmclassifier/data_model.py
@@ -1,6 +1,6 @@
 from typing import List, Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 
 def generate_multi_class_classification_model(list_classes: list[str]):
@@ -16,7 +16,13 @@ def generate_multi_label_classification_model(list_classes: list[str]):
     assert list_classes
 
     class ClassificationOutput(BaseModel):
-        category: List[Literal[tuple(list_classes)]]
+        category: List[Literal[tuple(list_classes)]] = Field(default_factory=list)
+
+    @field_validator('category', mode='before')
+    def filter_invalid_categories(cls, value):
+        if isinstance(value, list):  # Ensure input is a list
+            return [v for v in value if v in list_classes]
+        return []  # Return an empty list if the input is not a list
 
     return ClassificationOutput
 
